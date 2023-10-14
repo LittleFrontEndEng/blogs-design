dea23d6840d78f9de36e59b6952468582b0d5cb4
liumangtu <1312483228@qq.com>
2023-10-14 19:05:43 +0800

    feat: add article editor

--- web_pro/src/pages/Article/Article.tsx
+++ web_pro/src/pages/Article/Article.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { Button } from 'antd'
+import { history } from 'umi'
 const Article = () => {
+
+  // 跳转创建页
+  const jumptoTarget = () => {
+    history.push({
+      pathname: '/article/createarticle',
+      // query: {
+      //   a: 'b',
+      // },
+    });
+  }
   return (
     <div>
       文章管理
+      <Button type='primary' onClick={jumptoTarget}>创建文章</Button>
     </div>
   )
 }
--- web_pro/src/pages/Article/components/CreateArticle/CreateArticle.tsx
+++ web_pro/src/pages/Article/components/CreateArticle/CreateArticle.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import MyEditor from '@/components/MyEditor/MyEditor'
 import styles from './CreateArticle.less'
 const CreateArticle = () => {
+  const [editor, setEditor] = useState(null);
+  const [html, setHtml] = useState('<p>hello</p>');
+  useEffect(() => {
+    console.log('编辑器', editor);
+  })
+
+  const getHtml = (html: string) => {
+    setHtml(html);
+  }
   return (
-    <div>
-      创建文章
+    <div className={styles.container}>
+      <div className={styles.editBox}>
+        <MyEditor title="<h2>标题</h2>" setValue='<p>123</p>' getHtml={getHtml}/>
+      </div>
+      <div className={styles.previewBox}>
+        <div style={{ marginTop: '15px' }} dangerouslySetInnerHTML={{__html: html}}></div>
+      </div>
     </div>
   )
 }
